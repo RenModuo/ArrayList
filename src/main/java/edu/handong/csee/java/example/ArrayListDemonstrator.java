8419d18c454f7c02bc18c5b18e7302934add6f92
RenModuo <jououdom@gmail.com>
2024-09-10 10:24:07 +0700

    l

--- src/main/java/edu/handong/csee/java/example/ArrayListDemonstrator.java
+++ src/main/java/edu/handong/csee/java/example/ArrayListDemonstrator.java
@@ -1,12 +1,12 @@
 package edu.handong.csee.java.example;
 /**
- *	Demonstrating ArrayList, Creating an item list with dynamic size through user input and displaying it in a list
+ *	Demonstrating ArrayList, Creating a list with dynamic size through user input and displaying it in a list
  */
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList;		//for use of ArrayList and its method
+import java.util.Scanner;		//For use of Scanner and its method
 /*
  * Precondition: Input items into the array or list through user input
- * Postcondition: Output list of items from array in order of input
+ * Postcondition: Output list of items from the list in order of input
  */
 public class ArrayListDemonstrator {
 
@@ -21,7 +21,7 @@ public class ArrayListDemonstrator {
 	}
 
 	private void run() {
-		//Create an object of ArrayList Class
+		//Create an object of ArrayList Class, is a list that is similar to an array as it uses index
 		ArrayList<String> toDoList = new ArrayList<String>();
         
 		System.out.println ("Enter items for the list, when prompted.");
@@ -32,7 +32,7 @@ public class ArrayListDemonstrator {
         //For input
 		Scanner keyboard = new Scanner (System.in); 
 		
-		//Loop for inputting elements into the array
+		//Loop for inputting elements into the list
 		while (!done) {
 			//Prompt user input
             System.out.println ("Type an entry:");
@@ -54,12 +54,12 @@ public class ArrayListDemonstrator {
 		//Display
 		System.out.println ("The list contains:");
         
-		//Getting the size of the array
+		//Getting the size of the list
 		int listSize = toDoList.size();
 		
-		//Looping to iterate through all elements of the array and displaying them
+		//Looping to iterate through all items of the list and displaying them
         for (int position = 0 ; position < listSize ; position++)
-            System.out.println (toDoList.get(position));	//Get the value of the element in a specific index
+            System.out.println (toDoList.get(position));	//Get the item in a specific index
         
         //Closes Scanner
         keyboard.close();
